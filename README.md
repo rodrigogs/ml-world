a5829bb7066bb78babda62323f0ed1c205f0f363
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2021-07-13 00:45:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# ml-world
+
+Base Unity project ready to use with [ml-agents](https://github.com/Unity-Technologies/ml-agents).
+
+## Instalation
+```ps1
+./Scripts/setup.ps1
+```
+
+## Usage
+```ps1
+mlagents-learn ./Assets/Configs/behaviors.yml --force --run-id MyBehavior
+```
---
+++ Scripts/setup.ps1
@@ -0,0 +1,18 @@
+$pythonVersion = &{python -V} 2>&1
+
+if ($pythonVersion -Match "Python 3.9*") {
+  echo "$pythonVersion found!"
+} else {
+  echo "$pythonVersion is invalid! Use 3.9 or greater."
+  exit 1
+}
+
+echo "Setting up environment..."
+python -m pip install virtualenv
+virtualenv -p python3.9 venv
+.\venv\Scripts\Activate.ps1
+python -m pip install --upgrade pip
+python -m pip install torch==1.7.1 -f https://download.pytorch.org/whl/torch_stable.html
+python -m pip install -r requirements.txt
+deactivate
+echo "Setup complete."
