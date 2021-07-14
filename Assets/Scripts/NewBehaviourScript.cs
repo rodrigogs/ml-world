5cac2ba69adf6710a2cd137f6eda8dff68eb885d
Rodrigo Gomes da Silva <rodrigo.smscom@gmail.com>
2021-07-13 22:13:49 -0300

    fixed ml-agents configuration

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class NewBehaviourScript : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
